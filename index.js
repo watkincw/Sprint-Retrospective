5bf8e86d790585194ae0ebfe1ed1fabc3970c5a7
Heidi <heidiw@marsreturnship.com>
2022-03-08 12:35:46 -0600

    Finish product -Jeyanthi/Heidi

--- index.js
+++ index.js
@@ -1,65 +1,77 @@
 // lucky number shhhhh
-const luckyNumber = 7;
+function getRandomInt(min, max) {
+  min = Math.ceil(1);
+  max = Math.floor(100);
+  return Math.floor(Math.random() * (100 - 1) + 1); //The maximum is exclusive and the minimum is inclusive
+}
+let luckyNumber = getRandomInt();
+
 // guess counter
 let counter = 0;
 
 // lucky number check function
 function checkNumber() {
+  // get input value(user's guess)
+  let number = Number(document.getElementById("guessedNumber").value);
 
-    // get input value(user's guess)
-    let number = Number(document.getElementById('guessedNumber').value);
+  if (number > 100 || number < 0 || number == "") {
+    // target the 'Bad Number' answer message
+    let hidden = document.getElementById("hiddenBadNumber");
 
-    if (number > 10 || number < 0 || number == '') {
-        // target the 'Bad Number' answer message
-        let hidden = document.getElementById('hiddenBadNumber');
+    // un-hide it
+    hidden.classList.remove("hide");
 
-        // un-hide it
-        hidden.classList.remove('hide');
-
-        // after 3 seconds(3000 milliseconds), re-hide it
-        setTimeout(function() {
-            hidden.classList.add('hide');
-        }, 3000);
+    // after 3 seconds(3000 milliseconds), re-hide it
+    setTimeout(function () {
+      hidden.classList.add("hide");
+    }, 3000);
 
     // if input number is the lucky number
-    } else if (number == luckyNumber) {
-        // target the 'Correct' answer message
-        let hidden = document.getElementById('hiddenCorrect');
-    
-        // un-hide it
-        hidden.classList.remove('hide');
-        
-        // after 3 seconds, re-hide it
-        setTimeout(function() {
-            hidden.classList.add('hide');
-        }, 3000);
+  } else if (number == luckyNumber) {
+    // target the 'Correct' answer message
+    let hidden = document.getElementById("hiddenCorrect");
 
-        // increment the guess counter
-        counter++;
+    // un-hide it
+    hidden.classList.remove("hide");
 
-        // display new guess count
-        document.getElementById('counter').innerHTML = counter;
+    // after 3 seconds, re-hide it
+    setTimeout(function () {
+      hidden.classList.add("hide");
+    }, 3000);
+
+    // increment the guess counter
+    counter++;
+
+    // display new guess count
+    document.getElementById("counter").innerHTML = counter;
+    luckyNumber = getRandomInt();
 
     // if the input number is NOT the lucky number
-    } else if (number != luckyNumber) {
-        // target the 'Wrong' answer message
-        let hidden = document.getElementById('hiddenWrong');
+  } else if (number != luckyNumber) {
+    if (number > luckyNumber) {
+      let hiddenHigh = document.getElementById("hiddenHigh");
+      hiddenHigh.classList.remove("hide");
 
-        // un-hide it
-        hidden.classList.remove('hide');
+      // after 3 seconds(3000 milliseconds), re-hide it
+      setTimeout(function () {
+        hiddenHigh.classList.add("hide");
+      }, 3000);
+    } else {
+      let hiddenLow = document.getElementById("hiddenLow");
+      hiddenLow.classList.remove("hide");
 
-        // after 3 seconds(3000 milliseconds), re-hide it
-        setTimeout(function() {
-            hidden.classList.add('hide');
-        }, 3000);
-
-        // increment the guess counter
-        counter++;
-
-        // display new guess count
-        document.getElementById('counter').innerHTML = counter;
-
-        console.log(counter);
-        console.log(document.getElementById('counter').value);
+      // after 3 seconds(3000 milliseconds), re-hide it
+      setTimeout(function () {
+        hiddenLow.classList.add("hide");
+      }, 3000);
     }
-};
+
+    // increment the guess counter
+    counter++;
+
+    // display new guess count
+    document.getElementById("counter").innerHTML = counter;
+
+    console.log(luckyNumber);
+  }
+}
